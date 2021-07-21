c985efe36c5d7bf5a5386f3e2e3d195eed7e5847
John Gibson <jg20019@gmail.com>
2021-07-20 20:07:47 -0400

    Make post.date into a date

--- post.py
+++ post.py
@@ -1,14 +1,14 @@
 from typing import Dict
-
+from datetime import date
 
 class Post:
     def __init__(self, filename, headers: Dict, body: str):
         self.title = headers['title'] 
         self.author = headers['author']
-        self.date = headers['date']
+        self.date = date.fromisoformat(headers['date'])
         self.filename = filename
 
         self.body = body
 
     def __str__(self) -> str: 
-        return f'{self.title} by {self.author}'
+        return f'{self.title} by {self.author}' 
